f3a3548a5bd65d3eedd0a23f9d5f1d53dfb6faf8
Катерина <77587548+KaterinaSavina@users.noreply.github.com>
2024-02-08 22:39:36 +0300

    Create Happy_number.py

---
+++ Happy_number.py
@@ -0,0 +1,27 @@
+def sum_of_square_digits(number, degree):
+    index = 0
+    str_number = str(number)
+    count_symbol = len(str_number)
+    happy_number = 0
+    new_symbol = 0
+    while (index < count_symbol):
+        symbol = pow(int((str_number)[index]), degree)
+        index = index + 1
+        happy_number = int(symbol) + int(new_symbol)
+        new_symbol = happy_number
+    return(happy_number)
+
+def is_happy_number(number):
+    degree = 2
+    iteration = 0
+    count_iteration = 10
+    happy_number = 0
+    while ((iteration < count_iteration) and (happy_number != 1)):
+        happy_number = sum_of_square_digits(number, degree)
+        iteration = iteration + 1
+        number = happy_number
+    if happy_number == 1:
+        return('true')
+    else:
+        return('false')
+print(is_happy_number(7))
